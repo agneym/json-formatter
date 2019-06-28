ec9e4105eeea91e609f2508fbfaef1df4366e965
Boy with Silver Wings <agney@dexlock.com>
2019-06-28 16:21:56 +0530

    rename to js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import "whatwg-fetch";
 
 function App() {
-  return <p>App</p>
+  return (
+    <p>App</p>
+  )
 }
 
 export default App;
---
+++ src/Editor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Editor() {
+  return (
+    <p>Editor</p>
+  )
+}
+
+export default Editor;

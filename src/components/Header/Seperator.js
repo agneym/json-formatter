16f7f3d46fc68cb09e067543bc74ccd5d7bf2299
Boy with Silver Wings <agney@outlook.in>
2019-07-11 22:20:12 +0530

    add load from url option

---
+++ src/components/Header/Seperator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Line = styled.hr`
+  display: block;
+  border: none;
+  height: 0.1rem;
+  border-bottom: 0.1rem solid ${props => props.theme.colors.grey};
+`;
+
+const Text = styled.p`
+  margin: 0;
+  position: absolute;
+  left: 50%;
+  top: 0;
+  transform: translate(-50%, -50%);
+  background-color: #FFFFFF;
+  padding: 0 0.5rem;
+  color: ${props => props.theme.colors.grey};
+`;
+
+function Seperator() {
+  return (
+    <div css={`position: relative; margin: 3rem;`}>
+      <Line />
+      <Text>OR</Text>
+    </div>
+  );
+}
+
+export default Seperator;
+
--- src/components/Header/UploadBanner.js
+++ src/components/Header/UploadBanner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
+import { Button, InputText } from "buffetjs";
 
 import UploadFile from './UploadFile';
+import Seperator from './Seperator';
 
-const Container = styled.div`
+const Container = styled.form`
   background-color: #FFFFFF;
-  height: 20rem;
+  min-height: 20rem;
   width: 30vw;
   box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
   padding: 1rem;
@@ -16,6 +18,9 @@ function UploadBanner() {
   return (
     <Container>
       <UploadFile />
+      <Seperator />
+      <InputText type="url" name="url" placeholder="Enter a JSON URL" css={`margin: 1rem 3rem; width: 85%;`} />
+      <Button type="submit" css={`margin: 1rem auto; text-align: center; display: block;`}>Load</Button>
     </Container>
   );
 }

c2e7282a94a4819e3a8b18ccfc7db0816e1d6adc
Boy with Silver Wings <agney@outlook.in>
2019-08-13 23:55:02 +0530

    create by link component

---
+++ src/components/Header/Banner/ShareBanner/ByLink.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ByLink = () => {
+  return (
+    <p>By Link</p>
+  );
+}
+
+export default ByLink;
--- src/components/Header/Banner/ShareBanner/index.js
+++ src/components/Header/Banner/ShareBanner/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Container from "../BannerContainer";
+import Seperator from "../Seperator";
+import ByLink from "./ByLink";
 
 const ShareBanner = () => {
   return (
     <Container>
-      <p>Share Banner</p>
+      <p>Download</p>
+      <Seperator />
+      <ByLink />
     </Container>
   );
 }

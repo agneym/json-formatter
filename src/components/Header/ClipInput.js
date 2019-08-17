797bc3c0563e16b9c17504aeff78769f00af7c73
Boy with Silver Wings <agney@outlook.in>
2019-08-17 23:52:45 +0530

    copy command on click

--- src/components/Header/ClipInput.js
+++ src/components/Header/ClipInput.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import PropTypes from "prop-types";
-import { InputText } from "buffetjs";
 import styled from "styled-components";
 
 import CopyIcon from "../../icons/copy.svg";
@@ -8,10 +7,11 @@ import CopyIcon from "../../icons/copy.svg";
 const Container = styled.div`
   display: flex;
   padding: 0 4rem;
+`;
 
-  &>div {
-    width: 100%;
-  }
+const Input = styled.input`
+  width: 100%;
+  padding: 0.5rem;
 `;
 
 const NakedButton = styled.button`
@@ -27,9 +27,18 @@ const NakedButton = styled.button`
 `;
 
 const ClipInput = ({ value }) => {
+  const inputRef = useRef(null);
+  const copy = useCallback(() => {
+    const inputEl = inputRef.current;
+    if(inputEl) {
+      inputEl.focus();
+      inputEl.select();
+      document.execCommand("copy");
+    }
+  }, []);
   return (
-    <Container>
-      <InputText name="url-input-readonly" value={value} readonly />
+    <Container onClick={copy}>
+      <Input name="url-input-readonly" value={value} readOnly ref={inputRef} />
       <NakedButton>
         <CopyIcon css={`width: 100%;`} />
       </NakedButton>

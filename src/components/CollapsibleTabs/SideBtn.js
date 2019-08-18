38a3530c100b1cd6c6fce4c5de4810486f34aaaa
Boy with Silver Wings <agney@outlook.in>
2019-08-18 12:33:49 +0530

    position the header container to right

---
+++ src/components/CollapsibleTabs/SideBtn.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SideBtn = styled.button`
+  -webkit-appearance: none;
+  background-color: ${props => props.theme.colors.teritiary};
+  color: #ffffff;
+  box-shadow: none;
+  border: none;
+  padding: 1rem;
+  text-align: center;
+  font-size: 1.6rem;
+`;
+
+export default SideBtn;
--- src/components/CollapsibleTabs/index.js
+++ src/components/CollapsibleTabs/index.js
@@ -2,20 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import SideBtn from "./SideBtn";
+
 const Aside = styled.aside`
   position: fixed;
   top: calc(${props => props.theme.layout.navHeight} + 4rem);
   right: 0;
 `;
 
+const HeaderContainer = styled.div`
+  transform: translate(35%, 50%) rotate(270deg);
+`;
+
 const CollapsibleTabs = ({ tabs }) => {
   return (
     <Aside>
+      <HeaderContainer>
       { tabs.map(({ header }, index) => (
-        <button key={index}>
+        <SideBtn key={index}>
           {header}
-        </button>
+        </SideBtn>
       ))}
+      </HeaderContainer>
     </Aside>
   );
 }

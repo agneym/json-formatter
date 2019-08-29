5ac8f4a38cda16a6a18e68e2682a2f3121cc584d
Boy with Silver Wings <agney@outlook.in>
2019-08-29 22:39:09 +0530

    correct animtion origin

--- src/components/CollapsibleTabs/Content.js
+++ src/components/CollapsibleTabs/Content.js
@@ -6,24 +6,25 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 25vw;
   height: 100%;
+  transform: scaleX(0);
+  transform-origin: right;
 `;
 
 const Content = ({ show, children }) => {
-  return show ? <Container>{children}</Container> : null;
-  // const slideLeftTransitions = useTransition(show, null, {
-  //   enter: { transform: "scaleX(1)" },
-  //   leave: { transform: "scaleX(0)" },
-  //   from: { transform: "scaleX(0)" },
-  // });
-  // const AnimatedContainer = animated(Container);
-  // return slideLeftTransitions.map(
-  //   ({ item, key, props }) =>
-  //     item && (
-  //       <AnimatedContainer style={props} key={key}>
-  //         {children}
-  //       </AnimatedContainer>
-  //     )
-  // );
+  const slideLeftTransitions = useTransition(show, null, {
+    enter: { transform: "scaleX(1)" },
+    leave: { transform: "scaleX(0)" },
+    from: { transform: "scaleX(0)" },
+  });
+  const AnimatedContainer = animated(Container);
+  return slideLeftTransitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <AnimatedContainer style={props} key={key}>
+          {children}
+        </AnimatedContainer>
+      )
+  );
 }
 
 Content.defaultProps = {

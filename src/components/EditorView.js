97104c0259294b4ec3da1e90e4600ef67c1a026c
Boy with Silver Wings <agney@outlook.in>
2019-08-30 00:22:59 +0530

    create diff editor and format

--- src/components/EditorView.js
+++ src/components/EditorView.js
@@ -9,11 +9,14 @@ import DiffEditor from "./DiffEditor";
 const EditorView = () => {
   const editorConfig = useContext(EditorContext);
   const [transformed, setTransformed] = useState(null);
+
   const onTransform = (transformCode) => {
     try {
-      const originalValue = JSON.parse(editorConfig.getValue());
-      const transformedValue = Function(`"use strict";return (${transformCode})`)()(originalValue);
-      setTransformed(transformedValue);
+      const originalValue = editorConfig.getValue();
+      const originalValueObj = JSON.parse(originalValue);
+      const transformedValue = Function(`"use strict";return (${transformCode})`)()(originalValueObj);
+      const transformedValueJson = JSON.stringify(transformedValue);
+      setTransformed(transformedValueJson);
     } catch(err) {
       console.error(err);
     }

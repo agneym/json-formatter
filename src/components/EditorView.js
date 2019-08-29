86eb6e72cb3b02375575e3824aac0868e56d9f27
Boy with Silver Wings <agney@outlook.in>
2019-08-29 22:43:36 +0530

    import use state

--- src/components/EditorView.js
+++ src/components/EditorView.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useState } from "react";
 
 import Editor from "./Editor";
 import DetectPaste from "./DetectPaste";

4804e2996e6d63902f6c253c421536ea2fdebf5f
Boy with Silver Wings <agney@outlook.in>
2019-08-31 00:56:36 +0530

    mock out monaco editor

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
 
 import App from "../components/App";
 

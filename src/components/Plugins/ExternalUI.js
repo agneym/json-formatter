1c93212a2c5e8376b090bcbfa40fa237c90edde2
Agney Menon <agney@outlook.in>
2019-10-19 00:19:10 +0530

    receive event from web component

--- src/components/Plugins/ExternalUI.js
+++ src/components/Plugins/ExternalUI.js
@@ -1,18 +1,37 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useContext, useRef } from "react";
 
 import pluginPropType from "./pluginType";
+import EditorContext from "../Editors/EditorContext";
 
 function ExternalUI({ details }) {
+  const editorConfig = useContext(EditorContext);
+  const elementRef = useRef();
+
+  function onTransform(event) {
+    const data = event.detail;
+    console.log(data, event);
+  }
+
   useEffect(() => {
     const script = document.createElement("script");
     script.setAttribute("src", details.url);
     document.head.appendChild(script);
   }, [details]);
 
+  useEffect(() => {
+    const elementNode = elementRef.current;
+    if (elementNode) {
+      elementNode.addEventListener("json-transform", onTransform);
+    }
+  }, []);
+
+  const value = editorConfig.getValue();
+
   const Tag = details.tagName;
+
   return (
     <Fragment>
-      <Tag />
+      <Tag data={value} ref={elementRef} />
     </Fragment>
   );
 }

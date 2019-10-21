e1e5f040b2550f87d931df0b2bb8dc0d32d65045
Boy with Silver Wings <agney@dexlock.com>
2019-10-21 16:52:02 +0530

    add loading indicator

--- src/components/Plugins/ExternalUI.js
+++ src/components/Plugins/ExternalUI.js
@@ -28,10 +28,12 @@ function ExternalUI({ details, onTransform }) {
       srcDoc={`
       <html>
         <head>
+          <style>.loader{position: fixed; top: 30%; left: 50%; transform: translate(-50%, -50%);}</style>
           <script src=${details.url}></script>
         </head>
         <body>
           <${details.tagName} data='${value}'></${details.tagName}>
+          <svg viewBox="0 0 64 64" class="loader" height="3rem" width="3rem"><g fill="none" stroke="#3B8BEB" fill-rule="evenodd" stroke-width="3"><circle cx="32" cy="32" r="23.9953"><animate attributeName="r" begin="0s" dur="2s" values="0;24" keyTimes="0;1" keySplines="0.1,0.2,0.3,1" calcMode="spline" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" begin="0s" dur="2s" values=".2;1;.2;0" repeatCount="indefinite"></animate></circle><circle cx="32" cy="32" r="19.4645"><animate attributeName="r" begin="-1s" dur="2s" values="0;24" keyTimes="0;1" keySplines="0.1,0.2,0.3,1" calcMode="spline" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" begin="-1s" dur="2s" values=".2;1;.2;0" repeatCount="indefinite"></animate></circle></g></svg>
           <script>
             function handleTransform(event) {
               const detail = event.detail;
@@ -40,6 +42,10 @@ function ExternalUI({ details, onTransform }) {
                 window.top.postMessage(eventDetail, "*");
               }
             }
+            function hideLoader() {
+              document.querySelector(".loader").style.display = "none";
+            }
+            window.addEventListener("load", hideLoader);
             const el = document.querySelector("${details.tagName}");  
             el.addEventListener("json-transform", handleTransform);
           </script>

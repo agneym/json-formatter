5d4911011e4f666d2fe159bb9106fe9d3a6f5575
Agney Menon <agney@outlook.in>
2019-12-10 11:11:29 +0530

    Add optional chaining & nullish coalesing

--- babel.config.js
+++ babel.config.js
@@ -6,19 +6,21 @@ const presets = [
         edge: "17",
         firefox: "60",
         chrome: "67",
-        safari: "11.1"
+        safari: "11.1",
       },
       useBuiltIns: "usage",
       corejs: 3,
-    }
+    },
   ],
-  "@babel/preset-react"
+  "@babel/preset-react",
 ];
 
 const plugins = [
   "@babel/plugin-proposal-object-rest-spread",
   "@babel/plugin-proposal-class-properties",
-  "babel-plugin-styled-components"
+  "babel-plugin-styled-components",
+  "@babel/plugin-proposal-nullish-coalescing-operator",
+  "@babel/plugin-proposal-optional-chaining",
 ];
 
 module.exports = { presets, plugins };
